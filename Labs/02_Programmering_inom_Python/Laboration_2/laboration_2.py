9dca8ec3c26e7c493959c2b4a2e6bae61b6dd74a
Patrik Hellgren <patrik.hellgren@iths.se>
2025-09-16 17:37:02 +0200

    Exercises 04 done.

--- Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
+++ Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
@@ -1,12 +1,32 @@
-# import numpy as np
-
-# data = np.loadtxt("C:\Users\hellg\OneDrive\Dokument\Python\python-programming-PATRIK-HELLGREN\Labs\02-Programmering-inom-Python\Laboration-2\datapoints.txt", delimiter=",")
-# x = data[:, 0]
-# y = data[:, 1]
-
-# print(data)
-
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("\python-programming-PATRIK-HELLGREN\Labs\02_Programmering_inom_Python\Laboration_2\datapoints.txt")
-print(df.head())
+data_path = r"python-programming-PATRIK-HELLGREN\Labs\02_Programmering_inom_Python\Laboration_2\datapoints.txt"
+
+df = pd.read_csv(data_path, delimiter=',')
+x = df["(width (cm)"]
+y = df[" height (cm)"]
+z = df[" label (0-pichu"]
+
+plt.scatter(x[z == 0], y[z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="o")
+plt.scatter(x[z == 1], y[z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="o")
+
+# data_path = r"python-programming-PATRIK-HELLGREN\Labs\02_Programmering_inom_Python\Laboration_2\testpoints.txt"
+# df2 = pd.read_csv(data_path, skiprows=1, header=None, names=["testdata"]) # engine="python"
+
+# pattern = r"\(\s*([+-]?(?:\d*\.\d+|\d+))\s*,\s*([+-]?(?:\d*\.\d+|\d+))\s*\)"
+
+# data_points = df2["testdata"].str.extract(pattern)
+# data_points.columns = ["Width", "Height"]
+# data_points = data_points.astype(float)
+# print(data_points)
+
+# plt.scatter(x[z == 0], y[z == 0], color="red", alpha=0.6, edgecolors="black", label="Pichu (test data)", marker="*")
+# plt.scatter(x[z == 1], y[z == 1], color="blue", alpha=0.6, edgecolors="black", label="Pikachu (test data)", marker="*")
+
+plt.xlabel("Width")
+plt.ylabel("Height")
+plt.title("Pichu eller Pikachu?")
+plt.grid(True)
+plt.legend()
+plt.show()
