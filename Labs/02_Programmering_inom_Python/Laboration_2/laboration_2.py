16253be7f317c45ac7a894e59466f9dcdc1f8ad9
Patrik Hellgren <patrik.hellgren@iths.se>
2025-09-18 11:16:30 +0200

    Labb 2: handled tr & te data + nearest neighbour.

--- Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
+++ Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
@@ -1,44 +1,76 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_path = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/datapoints.txt"
-clean_content = []
+data_path_training = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/datapoints.txt"
+data_path_test = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/testpoints.txt"
+clean_content_tr = []
+clean_content_te = []
 
-with open(data_path, "r") as f:
-    next(f) # skip first row (headers)
-    for data in f:
-        split_content = data.split(",")
-        row = [float(d.strip().replace("(", "").replace(")", "")) for d in split_content]
-        clean_content.append(row)
+# TRAINING DATA (tr)
+with open(data_path_training, "r") as tr:
+    next(tr) # skip first row (headers)
+    for tr_data in tr:
+        split_content_tr = tr_data.split(",")
+        tr_row = [float(d.strip().replace("(", "").replace(")", "")) for d in split_content_tr]
+        clean_content_tr.append(tr_row)
 
-x, y, z = zip(*clean_content)
-x, y, z = np.array(x), np.array(y), np.array(z)
+tr_x, tr_y, tr_z = zip(*clean_content_tr)
+tr_x, tr_y, tr_z = np.array(tr_x), np.array(tr_y), np.array(tr_z)
 
-plt.scatter(x[z == 0], y[z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="o")
-plt.scatter(x[z == 1], y[z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="o")
-    
-plt.xlabel("Width")
-plt.ylabel("Height")
-plt.title("Pichu eller Pikachu?")
-plt.grid(True)
-plt.legend()
-plt.show()
+plt.scatter(tr_x[tr_z == 0], tr_y[tr_z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="o")
+plt.scatter(tr_x[tr_z == 1], tr_y[tr_z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="o")
 
-#content = pd.read_csv(data_path, delimiter=',')
-# x = f["(width (cm)"]
-# y = f[" height (cm)"]
-# z = f[" label (0-pichu"]
+# TEST DATA (te)
+# ta bort alla prints när labben är klar
+with open(data_path_test, "r") as te:
+    next(te) # skip first row (header)
+    for te_data in te:
+        te_data = te_data.split(" ", 1)[1]
+        split_content_te = [float(d.strip().replace("(", "").replace(")", "")) for d in te_data.split(",")]
+        clean_content_te.append(split_content_te)
 
-# data_path = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/testpoints.txt"
-# df2 = pd.read_csv(data_path, skiprows=1, header=None, names=["testdata"]) # engine="python"
+print(clean_content_te)
 
-# pattern = r"\(\s*([+-]?(?:\d*\.\d+|\d+))\s*,\s*([+-]?(?:\d*\.\d+|\d+))\s*\)" # FEL!
+te_x, te_y = zip(*clean_content_te)
+print(f"te_x, no np array: {te_x}")
+print(f"te_y, no np array: {te_y}")
+te_x, te_y = np.array(te_x), np.array(te_y)
+print(f"te_x, np array: {te_x}")
+print(f"te_y, np array: {te_y}")
 
-# data_points = df2["testdata"].str.extract(pattern)
-# data_points.columns = ["Width", "Height"]
-# data_points = data_points.astype(float)
-# print(data_points)
+# NEAREST NEIGHBOUR
+# ta bort alla prints när labben är klar
+tr_points = np.column_stack((tr_x, tr_y))
+print(f"tr_points:\n{tr_points}")
+tr_labels = tr_z
+print(f"tr_labels:\n{tr_labels}")
+te_points = np.column_stack((te_x, te_y))
+print(f"te_points:\n{te_points}")
 
-# plt.scatter(x[z == 0], y[z == 0], color="red", alpha=0.6, edgecolors="black", label="Pichu (test data)", marker="*")
-# plt.scatter(x[z == 1], y[z == 1], color="blue", alpha=0.6, edgecolors="black", label="Pikachu (test data)", marker="*")
+predicitons = []
 
+for te_point in te_points:
+    diff_x_y = tr_points[:, np.newaxis, :] - te_points[np.newaxis, :, :]
+    distances = np.sqrt(np.sum(diff_x_y ** 2, axis=2))
+    nearest_id = np.argmin(distances, axis=0)
+    prediction_label = tr_labels[nearest_id]
+    predicitons.append(prediction_label)
+
+print(f"distances:\n{distances}")
+print(f"nearest_id:\n{nearest_id}")
+print(f"prediction_label:\n{prediction_label}")
+print(f"predictions, no np array:\n{predicitons}")
+predicitons = np.array(predicitons)
+print(f"predictions, np array:\n{predicitons}")
+
+# PLOT
+
+#plt.scatter(te_x[te_z == 0], te_y[te_z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="*")
+#plt.scatter(te_x[te_z == 1], te_y[te_z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="*")
+
+# plt.xlabel("Width")
+# plt.ylabel("Height")
+# plt.title("Pichu eller Pikachu?")
+# plt.grid(True)
+# plt.legend()
+# plt.show()
