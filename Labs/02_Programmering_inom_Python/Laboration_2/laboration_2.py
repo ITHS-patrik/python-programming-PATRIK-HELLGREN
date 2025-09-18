9468b37f3e707bafb2059970b107c04d8e573eba
Patrik Hellgren <patrik.hellgren@iths.se>
2025-09-18 12:50:16 +0200

    Plotted test data.

--- Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
+++ Labs/02_Programmering_inom_Python/Laboration_2/laboration_2.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_path_training = "Labs/02_Programmering_inom_Python/Laboration_2/datapoints.txt"
-data_path_test = "Labs/02_Programmering_inom_Python/Laboration_2/testpoints.txt"
+data_path_training = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/datapoints.txt"
+data_path_test = "python-programming-PATRIK-HELLGREN/Labs/02_Programmering_inom_Python/Laboration_2/testpoints.txt"
 clean_content_tr = []
 clean_content_te = []
 
@@ -17,11 +17,7 @@ with open(data_path_training, "r") as tr:
 tr_x, tr_y, tr_z = zip(*clean_content_tr)
 tr_x, tr_y, tr_z = np.array(tr_x), np.array(tr_y), np.array(tr_z)
 
-plt.scatter(tr_x[tr_z == 0], tr_y[tr_z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="o")
-plt.scatter(tr_x[tr_z == 1], tr_y[tr_z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="o")
-
 # TEST DATA (te)
-# ta bort alla prints när labben är klar
 with open(data_path_test, "r") as te:
     next(te) # skip first row (header)
     for te_data in te:
@@ -29,23 +25,13 @@ with open(data_path_test, "r") as te:
         split_content_te = [float(d.strip().replace("(", "").replace(")", "")) for d in te_data.split(",")]
         clean_content_te.append(split_content_te)
 
-print(clean_content_te)
-
 te_x, te_y = zip(*clean_content_te)
-print(f"te_x, no np array: {te_x}")
-print(f"te_y, no np array: {te_y}")
 te_x, te_y = np.array(te_x), np.array(te_y)
-print(f"te_x, np array: {te_x}")
-print(f"te_y, np array: {te_y}")
 
 # NEAREST NEIGHBOUR
-# ta bort alla prints när labben är klar
 tr_points = np.column_stack((tr_x, tr_y))
-print(f"tr_points:\n{tr_points}")
 tr_labels = tr_z
-print(f"tr_labels:\n{tr_labels}")
 te_points = np.column_stack((te_x, te_y))
-print(f"te_points:\n{te_points}")
 
 predictions = []
 
@@ -56,28 +42,26 @@ for te_point in te_points:
     prediction_label = tr_labels[nearest_id]
     predictions.append(prediction_label)
 
-print(f"distances:\n{distances}")
-print(f"nearest_id:\n{nearest_id}")
-print(f"prediction_label:\n{prediction_label}")
-print(f"predictions, no np array:\n{predictions}")
-predictions = np.array(predictions)
-print(f"predictions, np array:\n{predictions}")
+predictions = np.array(predictions).ravel()
 
-# TEST DATA CONCLUSION
-print(te_x)
-print(te_y)
+# TEST DATA CLASSIFICATION
 for i in range(len(te_x)):
     pokemon = "Pichu" if predictions[i] == 0 else "Pikachu"
     print(f"Sample with (width, height): ({te_x[i]}, {te_y[i]}) classified as {pokemon}")
 
 # PLOT
 
-#plt.scatter(te_x[te_z == 0], te_y[te_z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="*")
-#plt.scatter(te_x[te_z == 1], te_y[te_z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="*")
+# Training data
+plt.scatter(tr_x[tr_z == 0], tr_y[tr_z == 0], color="green", alpha=0.6, edgecolors="black", label="Pichu", marker="o")
+plt.scatter(tr_x[tr_z == 1], tr_y[tr_z == 1], color="yellow", alpha=0.6, edgecolors="black", label="Pikachu", marker="o")
 
-# plt.xlabel("Width")
-# plt.ylabel("Height")
-# plt.title("Pichu eller Pikachu?")
-# plt.grid(True)
-# plt.legend()
-# plt.show()
+# Test data
+plt.scatter(te_x[predictions == 0], te_y[predictions == 0], color="green", edgecolors="black", label="Pichu (test)", marker="^")
+plt.scatter(te_x[predictions == 1], te_y[predictions == 1], color="yellow", edgecolors="black", label="Pikachu (test)", marker="^")
+
+plt.xlabel("Width")
+plt.ylabel("Height")
+plt.title("Pichu eller Pikachu?")
+plt.grid(True)
+plt.legend()
+plt.show()
