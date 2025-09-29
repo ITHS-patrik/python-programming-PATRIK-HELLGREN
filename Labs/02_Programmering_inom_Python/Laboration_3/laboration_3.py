2b2cb55726943734970ca2238cdb50f1c1a65c31
Patrik Hellgren <patrik.hellgren@iths.se>
2025-09-29 16:10:55 +0200

    Worked on different solutions for Labb 3.

--- Labs/02_Programmering_inom_Python/Laboration_3/laboration_3.py
+++ Labs/02_Programmering_inom_Python/Laboration_3/laboration_3.py
@@ -1,61 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.cluster import KMeans
 from pathlib import Path
 
 current_dir = Path(__file__).parent
 data = pd.read_csv(current_dir/"unlabelled_data.csv", header=None, names=["x", "y", "Classification"])
+x = np.array(data["x"])
+y = np.array(data["y"])
 
-def divide_into_clusters(data):
-    data_points = data[["x", "y"]].values
-    #print(data_points)
-    kmeans = KMeans(n_clusters=2, random_state=1337)
-    clusters = kmeans.fit_predict(data_points)
-    #print(f"clusters: {clusters}")
-    data["Classification"] = clusters
-    #print(data)
-    return data, clusters
+def calculate_line_and_classify(k, x, m, data=None):
 
-def calculate_line(data): # utan scikit-learn (LogisticRegression)! Använda?
-    
-    # Split the xy values between cluster 1 and 2 depending on their label.
-    cluster_1 = data[data["Classification"] == 0][["x", "y"]].values
-    cluster_2 = data[data["Classification"] == 1][["x", "y"]].values
+    line = k * x + m # om x = 0 -> line = m -> linjen skär y på m när x = 0.
 
-    # Calculate the mean of every cluster.
-    mean_cluster_1 = cluster_1.mean(axis=0)
-    mean_cluster_2 = cluster_2.mean(axis=0)
+    if data is not None:
+        data["Classification"] = (data["y"] > line).astype(int)
+        
+        return line, data
 
-    # Calculate direction (the vector from midpoint 0 to midpoint 1).
-    direction = mean_cluster_2 - mean_cluster_1 # (x2-x1), (y2-y1)
+    return line
 
-    # Calculate the midpoint between the two cluster midpoints.
-    midpoint = (mean_cluster_1 + mean_cluster_2) / 2 # (x1+x2)/2, (y1+y2)/2
+def create_csv(data):
 
-    # The dot product (skalärprodukten) between two vectors.
-    b = -np.dot(direction, midpoint)
+    try:
+        with open(current_dir/"labelled_data.csv", "w", newline="") as labelled_data:
+            labelled_data.write(data.to_csv(index=False, header=["x", "y", "Classification"]))
+    except OSError as err:
+        print(f"Something went wrong while writing to file: {err}.")
 
-    # Calculate k and m.
-    k = -direction[0] / direction[1]
-    m = -b / direction[1]
-    x = np.linspace(-6, 6, 100)
-    y = k * x + m
-    print(f"Line: y = {k:.2f}*x+{m:.2f}")
+line_ph, labelled_data_ph = calculate_line_and_classify(-1.05, x, 0, data) # bara höfta -1.05??
+line_fx, labelled_data_fx = calculate_line_and_classify(-0.489, x, 0, data)
+line_gx, labelled_data_gx = calculate_line_and_classify(-2, x, 0.16, data)
+line_hx, labelled_data_hx = calculate_line_and_classify(800, x, -120, data)
+# -> Jämföra labelled data ph, fx, gx och hx. Hur? Skapa flera figures -> fig1.scatter -> byt ut labelled_data_* för färgsättning?
+create_csv(labelled_data_ph)
 
-    return mean_cluster_1, mean_cluster_2, midpoint, y, x
-
-data_with_classification, clusters = divide_into_clusters(data)
-#print(f"With Classification:\n {data_with_classification}")
-mean_cluster_1, mean_cluster_2, midpoint, y, x = calculate_line(data_with_classification)
-
-plt.scatter(data.iloc[:, 0], data.iloc[:, 1], edgecolors="black", alpha=0.6, color=["green" if i == 0 else "blue" for i in clusters])
-plt.scatter(*mean_cluster_1, color="cyan", edgecolors="black", label="Midpoint in cluster 1")
-plt.scatter(*mean_cluster_2, color="yellow", edgecolors="black", label="Midpoint in cluster 2")
-plt.scatter(*midpoint, color="red", marker="*", s=100, label="Midpoint between clusters")
-
-plt.plot(x, y, color="black", linestyle="--", label="Line")
+plt.scatter(data["x"], data["y"], edgecolors="black", alpha=0.6, color=["green" if i == 0 else "blue" for i in labelled_data_ph["Classification"]])
+plt.plot(x, line_ph, color="black", label="My line = -1.05x", linewidth=1)
+plt.plot(x, line_fx, color="red", label="f(x) = -0.489x", linewidth=1)
+plt.plot(x, line_gx, color="green", label="g(x) = -2x + 0.16", linewidth=1)
+plt.plot(x, line_hx, color="blue", label="h(x) = 800x - 120", linewidth=1)
 plt.title("Laboration 3", fontweight="bold")
 plt.ylim(-6,6)
 plt.xlim(-6,6)
