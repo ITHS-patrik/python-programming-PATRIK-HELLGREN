59b57c04b208b3d363ac99cef2179d06f5063bec
Patrik Hellgren <patrik.hellgren@iths.se>
2025-10-01 14:27:07 +0200

    Solved the issue where classification was being overwritten in the CSV-file while comparing lines.

--- Labs/02_Programmering_inom_Python/Laboration_3/laboration_3.py
+++ Labs/02_Programmering_inom_Python/Laboration_3/laboration_3.py
@@ -4,37 +4,36 @@ import numpy as np
 from pathlib import Path
 
 current_dir = Path(__file__).parent
-data = pd.read_csv(current_dir/"unlabelled_data.csv", header=None, names=["x", "y", "Classification"])
+data = pd.read_csv(current_dir/"unlabelled_data.csv", header=None, names=["x", "y"])
 x = np.array(data["x"])
 y = np.array(data["y"])
 
-def calculate_line_and_classify(k, x, m, data=None):
+def calculate_line_and_classify(k, x, m, data):
+    df = data.copy() # creates a copy to not overwrite the original DataFrame later on during line comparision.
 
-    line = k * x + m # om x = 0 -> line = m -> linjen skär y på m när x = 0.
+    line = k * x + m
+    df["Classification"] = (df["y"] > line).astype(int)
 
-    if data is not None:
-        data["Classification"] = (data["y"] > line).astype(int)
-        
-        return line, data
-
-    return line
+    return line, df
 
 def create_csv(data):
 
     try:
-        with open(current_dir/"labelled_data.csv", "w", newline="") as labelled_data:
-            labelled_data.write(data.to_csv(index=False, header=["x", "y", "Classification"]))
+        data.to_csv(current_dir/"labelled_data.csv", index=False)
     except OSError as err:
         print(f"Something went wrong while writing to file: {err}.")
 
-line_ph, labelled_data_ph = calculate_line_and_classify(-1.05, x, 0, data) # bara höfta -1.05??
+line_ph, labelled_data_ph = calculate_line_and_classify(-1.05, x, 0, data)
 line_fx, labelled_data_fx = calculate_line_and_classify(-0.489, x, 0, data)
 line_gx, labelled_data_gx = calculate_line_and_classify(-2, x, 0.16, data)
 line_hx, labelled_data_hx = calculate_line_and_classify(800, x, -120, data)
+
 # -> Jämföra labelled data ph, fx, gx och hx. Hur? Skapa flera figures -> fig1.scatter -> byt ut labelled_data_* för färgsättning?
+# -> Jämföra antalet punkter under/över linjen mellan de olika linjerna? Typ count += 1 per linje?
+
 create_csv(labelled_data_ph)
 
-plt.scatter(data["x"], data["y"], edgecolors="black", alpha=0.6, color=["green" if i == 0 else "blue" for i in labelled_data_ph["Classification"]])
+plt.scatter(x, y, edgecolors="black", alpha=0.6, color=["green" if i == 0 else "blue" for i in labelled_data_ph["Classification"]])
 plt.plot(x, line_ph, color="black", label="My line = -1.05x", linewidth=1)
 plt.plot(x, line_fx, color="red", label="f(x) = -0.489x", linewidth=1)
 plt.plot(x, line_gx, color="green", label="g(x) = -2x + 0.16", linewidth=1)
