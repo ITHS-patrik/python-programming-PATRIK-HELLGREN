9b7ad2c83438d9d992a60d1eb916711187ea1770
Patrik Hellgren <patrik.hellgren@iths.se>
2025-10-18 00:32:41 +0200

    Finished all exercises and updated some that needed another look.

--- Explorations/test.py
+++ Explorations/test.py
@@ -3,23 +3,27 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 x = np.linspace(0, 20, 250)
+x2 = np.linspace(-10, 10, 250)
 
 fig = plt.figure(figsize=(16,5), dpi=100)
 grid_pos = gridspec.GridSpec(2, 4, width_ratios=[2,2,2,2], height_ratios=[1,1])
 div_x =     fig.add_subplot(grid_pos[0,0])
 sqrt =      fig.add_subplot(grid_pos[1,0])
 squared =   fig.add_subplot(grid_pos[0,1])
-yequalsx =  fig.add_subplot(grid_pos[1,1])
-cos =       fig.add_subplot(grid_pos[0,2])
-sin =       fig.add_subplot(grid_pos[1,2])
-tan =       fig.add_subplot(grid_pos[0,3])
+squaredodd = fig.add_subplot(grid_pos[1,1])
+yequalsx =  fig.add_subplot(grid_pos[0,2])
+cos =       fig.add_subplot(grid_pos[1,2])
+sin =       fig.add_subplot(grid_pos[0,3])
+tan =       fig.add_subplot(grid_pos[1,3])
 
 div_x.plot      (x, [1/i for i in x], label="1/x")
 div_x.legend()
 sqrt.plot       (x, np.sqrt(x), label="Square root")
 sqrt.legend()
-squared.plot    (x, np.square(x), label="Squared (power of)")
+squared.plot    (x2, np.square(x2), label="Squared even (U-shape) (power of)")
 squared.legend()
+squaredodd.plot (x2, x ** 3, label="Squared odd (S-shape) (power of)")
+squaredodd.legend()
 yequalsx.plot   (x, [i for i in x], label="y = x")
 yequalsx.legend()
 
